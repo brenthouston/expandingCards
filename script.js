e82ea2ff425c62517d57c3e702644d69ddf357d7
Brent Houston <brentjustinhouston@gmail.com>
2023-08-05 21:58:55 -0600

    begining to modularize

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-var inFocus = document.querySelectorAll(".img")
+import IMG from "./IMG.js";
 
+// var inFocus = document.querySelectorAll(".img");
 
-function focus(e){
-    e.currentTarget.setAttribute("style","width: auto")
-   
-}
+// function focus(e){
+//     if (e.currentTarget.value === "false"){
+//         e.currentTarget.setAttribute("style","width: auto")
+//         e.currentTarget.setAttribute("value", "true")
 
-inFocus.forEach(function(element){
-    element.addEventListener('click', focus);
-   
-})
+//     }else{
+//         e.currentTarget.setAttribute("style","width: 100px")
+//         e.currentTarget.setAttribute("value", "false")
+//     }
+
+// }
+
+// inFocus.forEach(function(element){
+
+//     element.addEventListener('click', focus);
+// })
+
+//Picture objects
+
+// function bannana(){
+//     var bananna = document.createElement("img")
+//     bananna.setAttribute("src", "images/bananna.jpg")
+//     bananna.setAttribute("width", "400")
+//     bananna.setAttribute("height", "400")
+//     bananna.setAttribute("alt", "Bananna")
+//     document.body.appendChild(bananna)
+
+// }
+
+const banImg = new IMG("400px", "400px", "../images/bananna.jpg");
+
+const main = document.querySelector("main");
+
+const content = `
+    <div>
+    <img src="${banImg.image}"></img>
+    </div>`;
+
+main.innerHTML = content;
